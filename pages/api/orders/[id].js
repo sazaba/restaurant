3246c01926dbe2f88c008e9c60578ce981587e61
Santiago Zappa <santiagozappab@gmail.com>
2022-11-20 20:45:30 -0500

    nextjs upload

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -15,8 +15,26 @@ export default async function handler(req, res) {
             res.status(500).json(error)
 
         }
-
     }
-    if (method === 'PUT') { };
-    if (method === 'DELETE') { };
+    if (method === 'PUT') {
+        try {
+            const order = await Order.findByIdAndUpdate(id, req.body, {
+                new: true,
+            });
+            res.status(200).json(order)
+
+        } catch (error) {
+            res.status(500).json(error)
+
+        }
+    };
+
+    if (method === 'DELETE') {
+        try {
+            await Order.findByIdAndDelete(id);
+            res.status(200).json('Orden removida')
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    };
 }
