213efe99fe652657a4f6d24b5bbdd01e0bc9127e
Santiago Zappa <santiagozappab@gmail.com>
2022-11-21 12:40:26 -0500

    testapi

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ pizzaList }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get('http://localhost:3000/api/products');
+  const res = await axios.get('http://restaurant-pearl-phi.vercel.app/api/products');
   return {
     props: {
       pizzaList: res.data
