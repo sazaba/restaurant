f0b7fdeeee4c02a4078d2a83149808f1f3c95eab
Santiago Zappa <santiagozappab@gmail.com>
2022-11-21 12:56:02 -0500

    endpoints

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -97,7 +97,7 @@ const Order = ({ order }) => {
 }
 
 export const getServerSideProps = async ({ params }) => {
-    const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+    const res = await axios.get(`http://restaurant-delta-roan.vercel.app/api/orders/${params.id}`);
     return {
         props: {
             order: res.data
